b90eb9394048f7dfb17d745c5bdb5f59da34558b
tony <what_50_@hotmail.com>
2014-06-02 15:47:16 -0500

    Comit necesario para un merge

--- conexion.php
+++ conexion.php
@@ -1,8 +1,13 @@
 <?php
 $host ="localhost";
 $user="root";
+<<<<<<< HEAD
 
 $pw="123456" ;
 $db ="tienda-php";
 
+=======
+$pw="Padilla.1" ;
+$db ="tienda-php";
+>>>>>>> ef192c8ac68a40315b7e1d4c2c83c5fa95be2d28
 ?>
